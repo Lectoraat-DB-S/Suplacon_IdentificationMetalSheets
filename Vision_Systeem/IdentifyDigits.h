bb3411d62a3d740e521a1ad1d3a1390afec7238c
GekkeBob123 <s1139896@student.windesheim.nl>
2024-04-20 00:16:39 +0200

    Creating IdentifyDigits

---
+++ Vision_Systeem/IdentifyDigits.h
@@ -0,0 +1,38 @@
+#ifndef IDENTIFY_DIGITS_H
+#define IDENTIFY_DIGITS_H
+
+#define MIN_DIGITS 0
+#define MAX_DIGITS 10
+
+#include <string>
+
+#include "HalconCpp.h"
+
+struct FoundDigit {
+	std::string value{};
+	double confidence{};
+	double row{};
+	double column{};
+};
+
+class IdentifyNumbers
+{
+
+private:
+	HalconCpp::HImage image;
+	HalconCpp::HRegion outlineDigits;
+	HalconCpp::HOCRMlp classifier;
+	FoundDigit foundDigits[MAX_DIGITS];
+
+public:
+	IdentifyNumbers(HalconCpp::HImage image, HalconCpp::HRegion outlineDigits, const char* fontName);
+
+	bool execute();
+
+	void print();
+
+	FoundDigit* getFoundDigits() { return foundDigits; };
+};
+
+#endif
+
