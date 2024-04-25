f931971d5a4fcf36716716bfa7203a423cb81454
GekkeBob123 <s1139896@student.windesheim.nl>
2024-04-25 06:18:01 +0200

    Toevoegen WERKENDE open62541pp OPC UA server

--- OPC_UA_Server/OPC_UA_Server.cpp
+++ OPC_UA_Server/OPC_UA_Server.cpp
@@ -1,20 +1,30 @@
-// OPC_UA_Server.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include "open62541pp/open62541pp.h"
+
+int main() {
+	opcua::Server server(4840 /* port */);
+
+	server.setApplicationName("open62541pp server example");
+	server.setApplicationUri("urn:open62541pp.server.application");
+	server.setProductUri("https://open62541pp.github.io");
+
+	// Add a variable node to the Objects node
+	auto parentNode = server.getObjectsNode();
+	auto myIntegerNode = parentNode.addVariable(
+		{ 1, "TheAnswer" },
+		"The Answer",
+		opcua::VariableAttributes{}
+		.setDisplayName({ "en-US", "The Answer" })
+		.setDescription({ "en-US", "Answer to the Ultimate Question of Life" })
+		.setDataType<int>()
+	);
+
+	// Write a value (attribute) to the node
+	myIntegerNode.writeValueScalar(42);
+
+	// Read the value (attribute) from the node
+	std::cout << "The answer is: " << myIntegerNode.readValueScalar<int>() << std::endl;
+
+	server.run();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
